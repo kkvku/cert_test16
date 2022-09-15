7787ff17395e7e8bdd8f532b2a9d2bba0c556558
Kirill <tttt@tttt.com>
2022-09-15 20:46:13 +0300

    cert

---
+++ proj.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.91.0"
+    }
+  }
+}
+
+provider "yandex" {
+  # Configuration options
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+resource "yandex_compute_instance" "test1-build" {
+  name = "test1-build"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-2.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "test:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "test2-prod" {
+  name = "test2-prod"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-2.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "test:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-2" {
+  name = "network2"
+}
+
+resource "yandex_vpc_subnet" "subnet-2" {
+  name           = "subnet2"
+  zone           = var.zone
+  network_id     = yandex_vpc_network.network-2.id
+  v4_cidr_blocks = ["192.168.88.0/24"]
+}
+
+output "internal_ip_address_test1-build" {
+  value = yandex_compute_instance.test1-build.network_interface.0.ip_address
+}
+
+output "internal_ip_address_test2-prod" {
+  value = yandex_compute_instance.test2-prod.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_test1-build" {
+  value = yandex_compute_instance.test1-build.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_test2-prod" {
+  value = yandex_compute_instance.test2-prod.network_interface.0.nat_ip_address
+}
